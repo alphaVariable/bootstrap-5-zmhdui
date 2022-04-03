2e3d00a4896de4df5cc478f98d02a0571c9b0ebb
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-03 17:13:22 +0800

    adds query fxns

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles.scss';
 import 'bootstrap/js/dist/collapse';
+
+
+// get data from json file
+import data from '/site_data/home_page.json';
+
+// utility functions for the app
+qs = (selector) => document.querySelector(selector);
+qsa = (selector) => document.querySelectorAll(selector);
+
+
+//inject data into the page
