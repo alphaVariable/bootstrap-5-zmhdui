be3f5a170cc3cb3fd702ed9ffb400fb5fc16e3ed
alphaVariable <vikasoffice81@gmail.com>
2022-04-03 18:02:21 +0800

    4 files modified

--- src/index.js
+++ src/index.js
@@ -12,10 +12,27 @@ function qsa(selector) {
   return document.querySelectorAll(selector);
 }
 
-//inject data into the page
+// set meta
+qs('meta[name="description"]').setAttribute(
+  'content',
+  data.data.doorsteps_home_page.meta_data.meta_description
+);
+qs('title').text = data.data.doorsteps_home_page.meta_data.meta_title;
 
+//inject data into the page
+// using classes as classes have lower specificity than ids, also reusable
 // using innerHTML for convenience, not a goo practice to be using on prod
 qs('.hero__h1').innerHTML = data.data.doorsteps_home_page.hero.heading_h1;
-
+qs('.hero__p').innerHTML = data.data.doorsteps_home_page.hero.copy;
+qs('.hero__img').setAttribute(
+  'src',
+  data.data.doorsteps_home_page.hero.image.url
+);
+console.log(data.data.doorsteps_home_page.hero.url);
+qs('.hero__cta').setAttribute(
+  'href',
+  data.data.doorsteps_home_page.hero.cta.url
+);
+qs('.hero__cta').text = data.data.doorsteps_home_page.hero.copy;
 
 console.table(data.data.doorsteps_home_page.hero.heading_h1);
